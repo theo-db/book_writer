a9f87e5e4e7ddd9e91ad3a3408521f8ee6e808a0
theo db <76433244+theo-db@users.noreply.github.com>
2021-01-15 16:06:31 +0000

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1,18 +1,25 @@
 files = [
     "books/a_christmas_carol.txt",
+    "books/a_study_in_scarlet.txt",
     "books/dr_jekyll_and_mr_hyde.txt",
+    "books/dracula.txt",
     "books/frankenstein.txt",
+    "books/gullivers_travels.txt",
     "books/hound_of_the_baskervilles.txt",
+    "books/moby_dick.txt",
     "books/oliver_twist.txt",
     "books/sherlock_holmes.txt",
     "books/time_machine.txt",
     "books/treasure_island.txt",
-    "books/wizard_of_oz.txt"
+    "books/war_of_the_worlds.txt",
+    "books/wizard_of_oz.txt",
     ]
 
 codes = "abcdefghijklmnopqrstuvwxyz \".,?!';:()/\\0123456789"
 chars_in = 100
 chars_out = 1
+
+random_range = False
 
 shape = [chars_in * len(codes),
          20,45,100,
@@ -23,10 +30,10 @@ import numpy, random, threading
 
 random.shuffle(files)
 
-network = Network.Network(shape)
+#network = Network.Network(shape)
+network = Network.loadNetwork("network.nnetwork")
 
 try:
-    #train one book at a time so the RAM won't hate me
     for filename in files:
         print("opening {}".format(filename))
         text = ""
@@ -48,18 +55,12 @@ try:
         iterations = 10
         total_iterations = 1000
         passed = -0.01
-        #for x in range(total_iterations):
-        #    v = x/total_iterations
-        #    if v > passed+0.01:
-        #        print("{}%".format(v*100))
-        #        passed += 0.01
-        #    data = []
-        #    output = []
-        #    for y in range(iterations):
-        range_start = random.randrange(0, len(text)-chars_in-chars_out-1 - iterations, iterations)
-        range_end = random.randrange(range_start, len(text)-chars_in-chars_out-1, iterations)
-        #so it isn't always in the same range
-        #normally would be range(0,len(text)-chars_in-chars_out-1,iterations)
+        if random_range:
+            range_start = random.randrange(0, len(text)-chars_in-chars_out-iterations - (iterations*2), iterations)
+            range_end = random.randrange(range_start, len(text)-chars_in-chars_out-iterations, iterations)
+        else:
+            range_start=0
+            range_end = len(text)-chars_in-chars_out-iterations
         for x in range(range_start, range_end, iterations):
             data = []
             output = []
@@ -68,12 +69,13 @@ try:
                 print("{}%".format(v*100))
                 passed += 0.01
             for y in range(iterations):
-                #index = random.randrange(len(text) - chars_in - chars_out)
                 index = x+y
                 input_data = text[index : index+chars_in]
                 desired_output_data = text[index+chars_in : index+chars_in+chars_out]
                 if len(desired_output_data) < chars_out:
                     desired_output_data += " "*(chars_out-len(desired_output_data))
+                if len(input_data) < chars_in:
+                    input_data += " "*(chars_in-len(input_data))
 
                 in_values = numpy.zeros(shape[0], numpy.float64)
                 for i in range(chars_in):
@@ -82,15 +84,7 @@ try:
 
                 out_values = numpy.zeros(shape[-1], numpy.float64)
                 for i in range(chars_out):
-                    try:
-                        ind = codes.index(desired_output_data[i])
-                    except:
-                        print("#################################################")
-                        print(i)
-                        print(desired_output_data)
-                        print(len(desired_outoput_data))
-                        print(desired_output_data[i])
-                        print("#################################################")
+                    ind = codes.index(desired_output_data[i])
                     out_values[i*len(codes) + ind] = 1.0
 
                 data.append(in_values)
@@ -98,14 +92,8 @@ try:
             t = threading.Thread(target=network.train, args=(data, output, iterations))
             t.start()
             t.join()
-except KeyboardInterrupt:
-    Network.saveNetwork(network, "network.nnetwork")
+except:
+    pass
 
-        
-        
-
-    
-    
-
-    
-    
+Network.saveNetwork(network, "network.nnetwork")
+print("saved")
